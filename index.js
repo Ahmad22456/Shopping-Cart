5135afa247ca064ac87f5f751e9dbe543120c9f2
Ahmad Fawzy <shadow0181456@gmail.com>
2023-08-17 13:41:23 +0300

    Final??

--- index.js
+++ index.js
@@ -1,18 +1,114 @@
-const cart = document.querySelector(".btn")
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready())
+} else {
+    ready()
+}
+
+function ready() {
+    const likeButton = document.getElementsByClassName('btn_like')
+    for (let i = 0; i < likeButton.length; i++) {
+        let like = likeButton[i];
+        like.addEventListener('click', function(event) {
+            like.classList.toggle('btn_like_color')
+        })
+    }
+
+    const removeItem = document.getElementsByClassName("btn_remove")
+    for (let i = 0; i < removeItem.length; i++) {
+        let button = removeItem[i];
+        button.addEventListener('click', function(event){
+            let buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.remove()
+            updateTotalPrice()
+        }) 
+    }
+
+    const addItem = document.getElementsByClassName('btn_add')
+    for (let i = 0; i < addItem.length; i++) {
+        let toCart = addItem[i];
+        toCart.addEventListener('click', function(event) {
+            let title = toCart.parentElement.parentElement.parentElement.getElementsByClassName('ps5_text')[0].innerText
+            let price = toCart.parentElement.parentElement.parentElement.getElementsByClassName('ps5_price')[0].getElementsByClassName('price_text')[0].innerText
+            addToCart(title, price)
+            updateTotalPrice()
+        }) 
+    }
+}
+
+function addToCart(title, price) {
+    let cartList = document.getElementsByClassName('cart_items')[0]
+    let cartListNames = cartList.getElementsByClassName('ps5_text')
+    for (let i = 0; i < cartListNames.length; i++) {
+        let names = cartListNames[i].innerText;
+        if(names == title) {
+            return
+        }
+    }
+    let createItem = document.createElement('div')
+    createItem.classList.add('items')
+    let itemBody = `
+    <p class="ps5_text">${title}</p>
+    <div class="ps5_price">
+     <p class="price_text">${price}</p> 
+     <p class="quantity_text quantity">Quantity: 1</p>
+     <button class="sub">-</button>
+     <button class="add">+</button>
+     <button class="btn_remove">Remove Item</button>
+    </div>`
+    createItem.innerHTML = itemBody
+    cartList.appendChild(createItem)
+    createItem.getElementsByClassName('add')[0].addEventListener('click', function(event) {
+        let quantity_value = event.target.parentElement.getElementsByClassName('quantity_text')[0];
+        console.log(quantity_value);
+        let currentQuantity = parseInt(quantity_value.innerText.replace('Quantity: ', ''));
+        let newQuantity = currentQuantity + 1;
+        quantity_value.innerText = `Quantity: ${newQuantity}`;
+        console.log(quantity_value.innerText);
+        updateTotalPrice();        
+    })
+
+    createItem.getElementsByClassName('sub')[0].addEventListener('click', function(event) {
+        let quantity_value = event.target.parentElement.getElementsByClassName('quantity_text')[0];
+        console.log(quantity_value);
+        let currentQuantity = parseInt(quantity_value.innerText.replace('Quantity: ', ''));
+        let newQuantity = currentQuantity - 1;
+        if(isNaN(newQuantity) || newQuantity <= 0) {
+            newQuantity = 1
+        }
+        quantity_value.innerText = `Quantity: ${newQuantity}`;
+        console.log(quantity_value.innerText);
+        updateTotalPrice();        
+    })
+
+    createItem.getElementsByClassName('btn_remove')[0].addEventListener('click', function(event) {
+        let buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateTotalPrice()
+    })
+}
+
+function updateTotalPrice() {
+    let cartItems = document.getElementsByClassName('cart_items')[0]
+    let itemsList = cartItems.getElementsByClassName('items')
+    let total = 0
+
+    for (let i = 0; i < itemsList.length; i++) {
+        let item = itemsList[i];
+        let item_price = item.getElementsByClassName('ps5_price')[0].getElementsByClassName('price_text')[0]
+        let item_quantity = item.getElementsByClassName('ps5_price')[0].getElementsByClassName('quantity_text')[0]
+        let price = parseFloat(item_price.innerText.replace(/Price:|,/g, ""))
+        let quantity = parseFloat(item_quantity.innerText.replace('Quantity: ', ''))
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('price')[0].innerText = 'Total price: ' + total
+}
+
 const shop = document.querySelector(".cart_img")
 const shade = document.querySelector(".shade")
 const kart = document.querySelector(".btn1")
 const list = document.querySelector(".cart_list")
-const item = document.querySelector(".items")
-const add = document.getElementsByClassName("btn_add")
-const remove = document.querySelector(".btn_remove")
-let price = document.querySelector(".price")
-let quantity = [0, 0]
-let counter = []
-let sum = 0
 
 document.addEventListener("click", function (event) {
-    // console.log(event.target)
     if(event.target == shop || event.target == shade) {
         return list.classList.toggle("hide")
     }
@@ -21,96 +117,3 @@ document.addEventListener("click", function (event) {
 kart.addEventListener("click", function (event) {
     list.classList.toggle("hide")
 })
-
-
-for (let i = 0; i < add.length; i++) {
-
-    let addButton = add[i];
-    let title = addButton.parentElement.parentElement.parentElement.getElementsByClassName("ps5_text")[0].innerHTML
-    let price = addButton.parentElement.parentElement.getElementsByClassName('price_text')[0].getElementsByClassName('price')[0].innerHTML
-
-    addButton.addEventListener('click', function(event){
-        addToCart(title, price)
-    })
-
-}
-
-function addToCart(title, price) {
-
-    let newItem = document.createElement('div')
-    newItem.classList.add('ps5')
-    let divbody = `
-                    <p class="ps5_text">${title}</p>
-                    <div class="ps5_price">
-                        <p class="price_text">Price:${price}</p> 
-                        <strong> Quantity = 0 </strong>
-                        <button class="btn_remove">Remove Item</button>
-                    </div>`
-    newItem.innerHTML = divbody
-    item.appendChild(newItem)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < add.length; i++) {
-
-//     counter[i] = 0
-//     let addButton = add[i];
-//     let description = addButton.parentElement.parentElement.parentElement.getElementsByClassName("ps5_text")[0].innerHTML
-    
-//     addButton.addEventListener("click", function (event) {
-
-//         if(counter[i] != 0) {
-//             return
-
-//         } else {
-
-//             let ps5 = document.createElement("div")
-//             ps5.classList.add(i)
-//             ps5.textContent = description + `                     Quantity ${quantity[i]}`
-//             item.appendChild(ps5)
-//         }
-
-//         counter[i]++
-//     })
-// }
-// for (let i = 0; i < add.length; i++) {
-
-//     let addButton = add[i];
-
-//     let description = addButton.parentElement.parentElement.parentElement.getElementsByClassName("ps5_text")[0].innerHTML
-
-//     addButton.addEventListener("click", function (event) {
-        
-//         if(description == "Sony Limited Edition Spider-Man 2 PS5 Console") {
-//             quantity[0] += 1
-//             sum += 30000
-//         } else if (description == "Microsoft Xbox Series X Console"){
-//             quantity[1]++
-//             sum += 25000
-//         }
-//         price.textContent = `Total price: ${sum}`
-//         ps5.textContent = description + `                     Quantity ${quantity[i]}`
-//     })
-// }
